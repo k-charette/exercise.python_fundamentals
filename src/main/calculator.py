d90643980d9d252924c55a49efdee0bdcdaca521
Ken Charette <charettekenneth@gmail.com>
2020-12-03 11:28:00 -0500

    Completed calculator assignment. Now testing for travis ci

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -1,13 +1,19 @@
 # Created by Leon Hunter at 9:54 AM 10/23/2020
 class Calculator(object):
     def add(self, a, b):
-        return None # TODO - Implement solution
+        total = a + b
+        return total # TODO - Implement solution
 
     def subtract(self, a, b):
-        return None # TODO - Implement solution
+        total = a - b
+        return total # TODO - Implement solution
 
     def multiply(self, a, b):
-        return None # TODO - Implement solution
+        total = a * b
+        return total # TODO - Implement solution
 
     def divide(self, a, b):
-        return None # TODO - Implement solution
+        total = a / b
+        return total # TODO - Implement solution
+
+
